2f271c0d6ede266f3bfc4a4849ff8140c628b95c
ken7253 <ken7253@gmail.com>
2022-07-11 23:32:44 +0900

    create custom template files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: '/assets/css/overwrite.css' },
+      ],
+    }
+  }
 })
