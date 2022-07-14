309f3bbd429254eab8f3c3e8c5af9d6414bda0fb
ken7253 <ken7253@gmail.com>
2022-07-15 03:40:53 +0900

    ページ一覧を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <h2>pages</h2>
+    <ul :class="$style.link">
+      <li :class="$style.item"><NuxtLink to="/works/">works</NuxtLink></li>
+      <li :class="$style.item"><NuxtLink to="/about/">about</NuxtLink></li>
+      <li :class="$style.item"><NuxtLink to="/post/">post</NuxtLink></li>
+    </ul>
     <zenn-article></zenn-article>
   </div>
 </template>
@@ -9,3 +15,16 @@ definePageMeta({
   title: ''
 })
 </script>
+
+<style module>
+.link {
+  display: flex;
+  list-style-type: none;
+}
+.item:not(:first-child)::before {
+  display: inline-block;
+  content: "/";
+  transform: translateY(3px);
+  padding: 0 3px;
+}
+</style>
