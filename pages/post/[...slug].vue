bd735492196c319bb2e3f6d3aa26470e1c0a913c
ken7253 <ken7253@gmail.com>
2022-07-25 00:56:09 +0900

    Revert "レイアウト調整"
    This reverts commit c4577b3ae8af435fd55f130b2fadf1e4082b0e13.

--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -2,15 +2,9 @@
   <div>
     <ContentDoc>
       記事データの取得に失敗しました。
-      <NuxtLink to="/" :class="$style.top">
+      <NuxtLink to="/">
         TOPに戻る
       </NuxtLink>
     </ContentDoc>
   </div>
 </template>
-
-<style module>
-.top {
-  display: block;
-}
-</style>
