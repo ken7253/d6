c4577b3ae8af435fd55f130b2fadf1e4082b0e13
ken7253 <ken7253@gmail.com>
2022-07-25 00:53:12 +0900

    レイアウト調整

--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -2,9 +2,15 @@
   <div>
     <ContentDoc>
       記事データの取得に失敗しました。
-      <NuxtLink to="/">
+      <NuxtLink to="/" :class="$style.top">
         TOPに戻る
       </NuxtLink>
     </ContentDoc>
   </div>
 </template>
+
+<style module>
+.top {
+  display: block;
+}
+</style>
