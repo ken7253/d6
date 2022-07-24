c40a4bfccf9e845f62907a44074ada678bd80a02
ken7253 <ken7253@gmail.com>
2022-07-25 00:56:14 +0900

    Revert "記事データの読み込みに失敗した場合の記述を追加"
    This reverts commit f2ce64f942a32f90208a36985af5b8d221b9a95c.

--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <ContentDoc>
-      記事データの取得に失敗しました。
-      <NuxtLink to="/">
-        TOPに戻る
-      </NuxtLink>
-    </ContentDoc>
+    <ContentDoc />
   </div>
 </template>
