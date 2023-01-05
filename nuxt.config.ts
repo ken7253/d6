c81c1465b3d6f5cff6813c012fbac1379a236233
ken7253 <ken7253@gmail.com>
2023-01-05 19:21:40 +0900

    コンテンツのロケール設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
     dataValue: 'theme',
   },
   content: {
+    defaultLocale: 'ja',
     // https://content.nuxtjs.org/api/configuration/
     highlight: {
       theme: 'github-dark-dimmed',
