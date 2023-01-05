5fa4b8f580ec883e5602ec3352b08494e92e74da
ken7253 <ken7253@gmail.com>
2023-01-05 20:20:36 +0900

    投稿日と一覧へのリンク追加

--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <ContentDoc />
-  </div>
+  <ContentDoc v-slot="{ doc }">
+    <ContentRenderer :value="doc" />
+    <div :class="$style['after-content']">
+      <span
+        >post at
+        <time v-if="doc.date" :datetime="doc.date">{{ doc.date.split('T')[0] }}</time></span
+      >
+      <NuxtLink href="/post">記事一覧へ戻る</NuxtLink>
+    </div>
+  </ContentDoc>
 </template>
 
 <script lang="ts" setup>
 import { useHead } from '#app';
-
 useHead({
   meta: [{ property: 'og:type', content: 'article' }],
 });
 </script>
+
+<style module>
+.after-content {
+  padding: 3rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
