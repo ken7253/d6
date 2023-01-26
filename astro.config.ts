c8488e320ef24bf1efb9156d4ae2bd6a19e85743
ken7253 <ken7253@gmail.com>
2023-01-27 00:41:30 +0900

    必要そうなインテグレーションをインストール

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+// integrations
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+
+// https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/ja/reference/configuration-reference/
   site: 'https://dairoku-studio.com',
+  integrations: [sitemap(), image()],
 });
