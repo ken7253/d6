1954ca8dd12af24e1199928021c76dd3cdc17fda
ken7253 <ken7253@gmail.com>
2023-01-27 00:01:59 +0900

    フォントをインストール

--- astro.config.ts
+++ astro.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // https://docs.astro.build/ja/reference/configuration-reference/
+  site: 'https://dairoku-studio.com',
 });
