d16f3e39fd63768a5cd091a591c7adbf613dacc4
ken7253 <ken7253@gmail.com>
2023-01-28 17:51:09 +0900

    `astro add lit`

--- astro.config.ts
+++ astro.config.ts
@@ -6,16 +6,19 @@ import image from '@astrojs/image';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/ja/reference/configuration-reference/
   site: 'https://dairoku-studio.com',
-  integrations: [sitemap(), image(), react()],
+  integrations: [sitemap(), image(), react(), lit()],
   server: {
-    host: true,
+    host: true
   },
   vite: {
     server: {
-      hmr: true,
-    },
-  },
+      hmr: true
+    }
+  }
 });
